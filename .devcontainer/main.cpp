9b99b8cab90db2146e3dde68920dfd33a3901860
Exorium <106205565+LMGamer-Studios@users.noreply.github.com>
2026-05-25 22:51:03 +0000

    first commit

---
+++ .devcontainer/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <cassert>
+#include <stdexcept>
+
+std::string lin = "------------------------------\n";
+
+int main() {
+
+	std::cout << lin << "Hola mundo!\n" << lin;
+	
+	return 0;
+}

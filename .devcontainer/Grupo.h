76909b0de60200911c8be2ef140ed61dba311de6
Exorium <106205565+LMGamer-Studios@users.noreply.github.com>
2026-06-10 17:51:25 -0600

    Refactor Grupo.h methods for partido registration and ranking

--- .devcontainer/Grupo.h
+++ .devcontainer/Grupo.h
@@ -50,24 +50,102 @@ class Grupo {
 		    cantSele++;
 		}
 		
-		void jugarPartido(Partido* partidoIn){
-			if (partidoIn == nullptr){
+		void registrarPartido(Partido* partidoIn){
+		    if (partidoIn == nullptr){
 		        throw std::invalid_argument("Error! Partido dado invalido!");
 		    }
-		    
+			
 		    if (cantPartido >= partidosMax){
 		        throw std::out_of_range("Error! Limite alcanzado!");
 		    }
-		    
-		    
+			
+		    Seleccion* loc = partidoIn->getSeleccionLocal();
+		    Seleccion* vis = partidoIn->getSeleccionVisitante();
+			
+		    bool locEstaEnElGrupo = false, visEstaEnElGrupo = false;
+			
+		    for (int i = 0; i < cantSele; i++){
+		        if (selecciones[i] == loc){
+		            locEstaEnElGrupo = true;
+		            break;
+		        }
+		    }
+			
+		    for (int i = 0; i < cantSele; i++){
+		        if (selecciones[i] == vis){
+		            visEstaEnElGrupo = true;
+		            break;
+		        }
+		    }
+			
+		    if (!locEstaEnElGrupo || !visEstaEnElGrupo){
+		        throw std::invalid_argument("Error! Uno o mas de los equipos NO esta en el grupo!");
+		    }
+			
+		    partidos[cantPartido] = partidoIn;
+		    cantPartido++;
+			
+		    loc->actualizarEstadistica(partidoIn->getEstadisticaLocal());
+		    vis->actualizarEstadistica(partidoIn->getEstadisticaVisitante());
 		}
 		
-		void tabla(/*inputs*/){
-			//logic
+		void tabla(){
+		
+		    if (cantSele == 0){
+		        throw std::logic_error("Error! No hay selecciones en el grupo");
+		    }
+		
+		    Seleccion* temp[4];
+		
+		    for (int i = 0; i < cantSele; i++){
+		        temp[i] = selecciones[i];
+		    }
+		
+		    for (int i = 0; i < cantSele; ++i) {
+		        for (int j = 0; j < cantSele - i - 1; ++j) {
+		            if (
+		                temp[j]->getPuntos() < temp[j + 1]->getPuntos() ||
+		
+		                (temp[j]->getPuntos() == temp[j + 1]->getPuntos() &&
+		                 temp[j]->getDiferenciaGoles() < temp[j + 1]->getDiferenciaGoles()) ||
+		
+		                (temp[j]->getPuntos() == temp[j + 1]->getPuntos() &&
+		                 temp[j]->getDiferenciaGoles() == temp[j + 1]->getDiferenciaGoles() &&
+		                 temp[j]->getGolesFavor() < temp[j + 1]->getGolesFavor())
+		            ){
+		                Seleccion* aux = temp[j];
+		                temp[j] = temp[j + 1];
+		                temp[j + 1] = aux;
+		            }
+		        }
+		    }
+		
+		    std::cout << "Tabla del grupo:\n";
+		
+		    for (int i = 0; i < cantSele; i++){
+		        std::cout << i+1 << ". "
+		                  << temp[i]->getNombrePais()
+		                  << " | Pts: " << temp[i]->getPuntos()
+		                  << " | DG: " << temp[i]->getDiferenciaGoles()
+		                  << " | GF: " << temp[i]->getGolesFavor() << "\n";
+		    }
 		}
 		
-		void getGanador(/*inputs*/){
-			//logic
+		Seleccion* getGanador(){
+		
+		    if (cantSele == 0){
+		        throw std::logic_error("Error! No hay selecciones en el grupo");
+		    }
+		
+		    Seleccion* mejor = selecciones[0];
+		
+		    for (int i = 1; i < cantSele; i++){
+		        if (selecciones[i]->getPuntos() > mejor->getPuntos()){
+		            mejor = selecciones[i];
+		        }
+		    }
+		
+		    return mejor;
 		}
 };
 

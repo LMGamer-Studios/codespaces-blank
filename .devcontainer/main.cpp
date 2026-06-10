03e45c6d700c82488553ca0f84b96ff70509d344
Exorium <106205565+LMGamer-Studios@users.noreply.github.com>
2026-06-10 01:12:28 +0000

    commitMessagePlaceholderPleaseReplaceInDueTime

--- .devcontainer/main.cpp
+++ .devcontainer/main.cpp
@@ -11,9 +11,63 @@
 std::string lin = "--------------------------------\n";
 
 int main() {
+	
+	int opcion=0;
+	
+	do{
+	std::cout<<"- - - - - - Bienvenido al sistema de simulacion de la copa del mundo 2026! - - - - - -"<<'\n';
+	std::cout<<'\n';
+	std::cout<<lin<<"Que accion desea realizar?"<<'\n'<<lin;
+	std::cout<<"1.Registrar grupos y selecciones"<<'\n';
+	std::cout<<'\n';
+	std::cout<<"2.Registrar resultados"<<'\n';
+	std::cout<<'\n';
+	std::cout<<"3.Mostrar tabla de posiciones"<<'\n';
+	std::cout<<'\n';
+	std::cout<<"4.Mostrar tabla de goleadores"<<'\n';
+	std::cout<<'\n';
+	std::cout<<"5.salir del programa"<<'\n';
+	std::cout<<lin<<"Dijite su selecion a continuacion: ";
+	std::cin>>opcion;
+	
+	switch(opcion){
+		case 1:
+			//Placeholder para el registro de Grupos
+			std::system("pause");
+			std::system("cls");
+			
+			break;
+		
+		case 2:
+			//Placeholder para el registro de resultados de un mismo grupo.
+			std::system("pause");
+			std::system("cls");
+			break;
+		case 3:
+			//Placeholder para mostrar y generar la tabla de posiciones de un grupo.
+			std::system("pause");
+			std::system("cls");
+			break;
+		case 4:
+			//Placeholder para mostrar y generar la tabla de goleadores de un torneo.
+			std::system("pause");
+			std::system("cls");
+			break;
+		case 5:
+			std::cout<<"Gracias por usar el programa! saliendo..."<<'\n';
+			std::system("pause");
+			std::system("cls");
+			break;
+		default:
+			std::cout<<"Opcion invalida, vuelva a intentarlo"<<'\n';
+			std::system("pause");
+			std::system("cls");
+	}
+	
+	}while(opcion!=5);
 
 	//std::system("cls");
-	std::cout << "\033[H\033[2J" << std::flush;
+	/*std::cout << "\033[H\033[2J" << std::flush;
 	
 	Director dt("Luis Enrique");
     Seleccion espana("Espania", dt);
@@ -23,9 +77,7 @@ int main() {
     
     Persona* p = espana.getPersona(1);
 	std::string nombre = p->getNombre();
-	std::cout << nombre << '\n';
-    std::cout << lin << "Seleccion: " << espana.getNombrePais() << '\n';
-    std::cout << "Jugadores registrados: " << espana.getCantidadPersonas() << '\n' << lin;
+	*/
 	
     return 0;
 }

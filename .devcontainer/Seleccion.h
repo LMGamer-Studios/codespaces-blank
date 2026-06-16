02c8adaf2e1e62c276b2971d85e1670bd8a65bd1
Exorium <106205565+LMGamer-Studios@users.noreply.github.com>
2026-06-16 14:01:21 -0600

    Implement copy constructor and assignment operator
    Added copy constructor and assignment operator for Seleccion class to manage deep copy of listaPersonas.

--- .devcontainer/Seleccion.h
+++ .devcontainer/Seleccion.h
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <string>
 #include <stdexcept>
+#include <cassert>
+
 #include "Jugador.h"
 #include "Director.h"
 #include "Estadistica.h"
@@ -34,7 +36,47 @@ public:
             listaPersonas[i] = nullptr;
         }
     }
-	
+
+    Seleccion(const Seleccion& other)
+        : nombrePais(other.nombrePais),
+          directorTecnico(other.directorTecnico),
+          cantidadActual(other.cantidadActual),
+          estadisticas(other.estadisticas)
+    {
+        for (int i = 0; i < jugMax; i++) {
+            listaPersonas[i] = nullptr;
+        }
+
+        for (int i = 0; i < cantidadActual; i++) {
+            listaPersonas[i] = new Jugador(*other.listaPersonas[i]);
+        }
+    }
+
+    Seleccion& operator=(const Seleccion& other) {
+
+        if (this == &other) return *this;
+
+        for (int i = 0; i < cantidadActual; i++) {
+            delete listaPersonas[i];
+            listaPersonas[i] = nullptr;
+        }
+
+        nombrePais = other.nombrePais;
+        directorTecnico = other.directorTecnico;
+        estadisticas = other.estadisticas;
+        cantidadActual = other.cantidadActual;
+
+        for (int i = 0; i < jugMax; i++) {
+            listaPersonas[i] = nullptr;
+        }
+
+        for (int i = 0; i < cantidadActual; i++) {
+            listaPersonas[i] = new Jugador(*other.listaPersonas[i]);
+        }
+
+        return *this;
+    }
+
     std::string getNombrePais() const {
         return nombrePais;
     }
@@ -42,88 +84,88 @@ public:
     int getCantidadPersonas() const {
         return cantidadActual;
     }
-    
+
     void agregarJugador(const std::string& nombre) {
-	
-	    if (nombre.empty()) {
-	        throw std::invalid_argument("Error! Nombre invalido!");
-	    }
-	
-	    if (cantidadActual >= jugMax) {
-	        throw std::out_of_range("Error! Limite alcanzado!");
-	    }
-	
-	    listaPersonas[cantidadActual] = new Jugador(nombre);
-	
-	    assert(listaPersonas[cantidadActual] != nullptr);
-	
-	    cantidadActual++;
-	}
-    
+
+        if (nombre.empty()) {
+            throw std::invalid_argument("Error! Nombre invalido!");
+        }
+
+        if (cantidadActual >= jugMax) {
+            throw std::out_of_range("Error! Limite alcanzado!");
+        }
+
+        listaPersonas[cantidadActual] = new Jugador(nombre);
+
+        assert(listaPersonas[cantidadActual] != nullptr);
+
+        cantidadActual++;
+    }
+
     Jugador* getPersona(int i) {
-	    if (i < 0 || i >= cantidadActual) {
-	        throw std::out_of_range("Jugador invalido");
-	    }
-	    return listaPersonas[i]; // asumiendo que guardar como: Jugador*
-	}
+        if (i < 0 || i >= cantidadActual) {
+            throw std::out_of_range("Jugador invalido");
+        }
+        return listaPersonas[i];
+    }
 
     bool perteneceElJugador(Jugador* jugIn) {
-	
-	    if (jugIn == nullptr) return false;
-	
-	    for (int i = 0; i < cantidadActual; i++) {
-	        assert(listaPersonas[i] != nullptr);
-	
-	        if (listaPersonas[i] == jugIn) {
-	            return true;
-	        }
-	    }
-	
-	    return false;
-	}
-	
-	void actualizarEstadistica(const Estadistica& est){
-	    estadisticas.acumular(est);
-	}
-	
-	int getPartidosJugados() const {
-		return estadisticas.getPartidosJugados();
-	}
-	
-	int getPartidosGanados() const {
-		return estadisticas.getPartidosGanados();
-	}
-	
-	int getPartidosEmpatados() const {
-		return estadisticas.getPartidosEmpate();
-	}
-	
-	int getPartidosPerdidos() const {
-		return estadisticas.getPartidosPerdidos();
-	}
-	
-	int getGolesFavor() const {
-		return estadisticas.getGolesFavor();
-	}
-	
-	int getGolesContra() const {
-		return estadisticas.getGolesContra();
-	}
-	
-	int getDiferenciaGoles() const {
-		return estadisticas.getDiferenciaGoles();
-	}
-	
-	int getPuntos() const {
-		return estadisticas.getPuntos();
-	}
-	
+
+        if (jugIn == nullptr) return false;
+
+        for (int i = 0; i < cantidadActual; i++) {
+            assert(listaPersonas[i] != nullptr);
+
+            if (listaPersonas[i] == jugIn) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    void actualizarEstadistica(const Estadistica& est){
+        estadisticas.acumular(est);
+    }
+
+    int getPartidosJugados() const {
+        return estadisticas.getPartidosJugados();
+    }
+
+    int getPartidosGanados() const {
+        return estadisticas.getPartidosGanados();
+    }
+
+    int getPartidosEmpatados() const {
+        return estadisticas.getPartidosEmpate();
+    }
+
+    int getPartidosPerdidos() const {
+        return estadisticas.getPartidosPerdidos();
+    }
+
+    int getGolesFavor() const {
+        return estadisticas.getGolesFavor();
+    }
+
+    int getGolesContra() const {
+        return estadisticas.getGolesContra();
+    }
+
+    int getDiferenciaGoles() const {
+        return estadisticas.getDiferenciaGoles();
+    }
+
+    int getPuntos() const {
+        return estadisticas.getPuntos();
+    }
+
     ~Seleccion() {
-	    for (int i = 0; i < cantidadActual; i++) {
-	        delete listaPersonas[i];
-	        listaPersonas[i] = nullptr;
-	    }
-	}
+        for (int i = 0; i < cantidadActual; i++) {
+            delete listaPersonas[i];
+            listaPersonas[i] = nullptr;
+        }
+    }
 };
 
 #endif
